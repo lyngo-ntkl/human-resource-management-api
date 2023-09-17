0cb749f187686ddd72cf3fa43b4812514062de07
Ly Ngô <lyngo.ntkl@gmail.com>
2023-09-17 17:21:24 +0700

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -41,11 +41,11 @@ builder.Services.AddScoped<ILeaveService, LeaveService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
